c8255252197807f965b320334106ebd76f7ed956
SergDzhus <SDzhoos@gmail.com>
2024-10-29 12:03:16 +0300

    Добавлен интерфейс.

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
 import smtplib
 from email.message import EmailMessage
+from tkinter import *
 
-sender_email = 'pythontest1234@yandex.ru'
-recipient_mail = 'qqq968@yandex.ru'
-password = 'uxezbjsxmdzqbbqp'
-subject = 'Проверка связи'
-body = 'Hi, from Python!'
 
-msg = EmailMessage()
-msg.set_content(body)
-msg['Subject'] = subject
-msg['From'] = sender_email
-msg['To'] = recipient_mail
+def send_email():
+    sender_email = 'pythontest1234@yandex.ru'
+    recipient_email = 'qqq968@yandex.ru'
+    password = 'uxezbjsxmdzqbbqp'
+    subject = 'Проверка связи'
+    body = 'Hi, from Python!'
 
-try:
-    server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
-    server.login(sender_email, password)
-    server.send_message(msg)
-    print('Mail sent!')
-except Exception as e:
-    print(f'Error - {e}')
-finally:
-    if server:
-        server.quit()
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+
+    server = None
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+        server.login(sender_email, password)
+        server.send_message(msg)
+        print('Mail sent!')
+    except Exception as e:
+        print(f'Error - {e}')
+    finally:
+        if server:
+            server.quit()
+
+window =Tk()
+window.title("Отправка e-mail")
+window.geometry('500x300')
+
+Label(text='Отправитель:').grid(row=0, column=0, sticky=W)
+sender_email_entry = Entry()
+sender_email_entry.grid(row=0, column=1, sticky=W)
+
+Label(text='Получатель:').grid(row=1, column=0, sticky=W)
+recipient_email_entry = Entry()
+recipient_email_entry.grid(row=1, column=1, sticky=W)
+
+Label(text='Пароль приложения:').grid(row=2, column=0, sticky=W)
+password_entry = Entry()
+password_entry.grid(row=2, column=1, sticky=W)
+
+Label(text='Тема письма:').grid(row=3, column=0, sticky=W)
+subject_entry = Entry()
+subject_entry.grid(row=3, column=1, sticky=W)
+
+Label(text='Cjj,otybt:').grid(row=4, column=0, sticky=W)
+body_text = Text(width=45, height=10)
+body_text.grid(row=4, column=1, sticky=W)
+
+Button(text="Отправить!", command=send_email).grid(row=5, column=1, sticky=W)
+
+result_label = Label(text='')
+result_label.grid(row=5, column=1, sticky=W)
+
+window.mainloop()
